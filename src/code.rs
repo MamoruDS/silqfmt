0d83c9812b5416b27786c43b3f3d9fdf88935f44
MamoruDS <mamoruds.io@gmail.com>
2020-08-04 22:49:18 +0900

    fixed two empty lines before real code in the output

--- src/code.rs
+++ src/code.rs
@@ -19,7 +19,7 @@ impl Code {
         }
     }
 
-    pub fn last_depth(&self) -> usize {
+    fn last_depth(&self) -> usize {
         *self.depths.last().unwrap()
     }
 
@@ -28,9 +28,12 @@ impl Code {
         self.depths.push(depth);
     }
 
-    pub fn read(&mut self, c: char) {
+    fn read(&mut self, c: char) {
         match c {
             '\n' => {
+                if self.lines.len() == 1 && self.lines.get(0).unwrap().len() == 0 {
+                    return;
+                }
                 self.newline(self.last_depth());
             }
             '{' => {
@@ -53,7 +56,11 @@ impl Code {
 
     pub fn format(&mut self) -> String {
         let code = self.raw.clone();
-        for c in code.chars() {
+        for (i, c) in code.chars().enumerate() {
+            if i < 100 && i > 90 {
+                println!("read c: {}", c);
+                println!("curl l: {:?}", self.lines);
+            }
             self.read(c);
         }
         self.code = String::new();
@@ -68,46 +75,46 @@ impl Code {
         let depth: usize = *self.depths.get(i).unwrap();
         let content = self.lines.get(i).unwrap();
 
-        if true {
-            match depth.cmp(&last_depth) {
-                Ordering::Equal => {
+        match depth.cmp(&last_depth) {
+            Ordering::Equal => {
+                if self.code.len() != 0 {
                     self.code.push('\n');
-                    self.code.push_str(&gen_indent(depth));
-                    self.code.push_str(content);
                 }
-                Ordering::Greater => {
-                    self.code.push(' ');
-                    self.code.push('{');
-                    match self.depths.get(i + 1) {
-                        Some(d) => match d.cmp(&depth) {
-                            Ordering::Less => {
-                                let c = self.lines.get(i + 1).unwrap();
-                                self.code.push('}');
-                                self.code.push('\n');
-                                self.code.push_str(&gen_indent(*d));
-                                self.code.push_str(match c.len() {
-                                    0 => " ",
-                                    _ => c,
-                                });
-                                self.format_line(i + 2, *d);
-                                return;
-                            }
-                            _ => {}
-                        },
+                self.code.push_str(&gen_indent(depth));
+                self.code.push_str(content);
+            }
+            Ordering::Greater => {
+                self.code.push(' ');
+                self.code.push('{');
+                match self.depths.get(i + 1) {
+                    Some(d) => match d.cmp(&depth) {
+                        Ordering::Less => {
+                            let c = self.lines.get(i + 1).unwrap();
+                            self.code.push('}');
+                            self.code.push('\n');
+                            self.code.push_str(&gen_indent(*d));
+                            self.code.push_str(match c.len() {
+                                0 => " ",
+                                _ => c,
+                            });
+                            self.format_line(i + 2, *d);
+                            return;
+                        }
                         _ => {}
-                    }
-                    self.code.push('\n');
-                    self.code.push_str(&gen_indent(depth));
-                    self.code.push_str(content);
+                    },
+                    _ => {}
                 }
-                Ordering::Less => {
-                    self.code.push('\n');
-                    self.code.push_str(&gen_indent(depth));
-                    self.code.push('}');
-                    self.code.push('\n');
-                    self.code.push_str(&gen_indent(depth));
-                    self.code.push_str(content);
-                }
+                self.code.push('\n');
+                self.code.push_str(&gen_indent(depth));
+                self.code.push_str(content);
+            }
+            Ordering::Less => {
+                self.code.push('\n');
+                self.code.push_str(&gen_indent(depth));
+                self.code.push('}');
+                self.code.push('\n');
+                self.code.push_str(&gen_indent(depth));
+                self.code.push_str(content);
             }
         }
         self.format_line(i + 1, depth)
