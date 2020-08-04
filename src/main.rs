c6a9150c7adaf462b39c1f54ed17acb5d9777894
MamoruDS <mamoruds.io@gmail.com>
2020-08-04 22:51:36 +0900

    modified args handling in main.rs has been moved to cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+mod cli;
 mod code;
 mod pattern;
 
+use code::Code;
 use std::{
-    env, fs,
+    fs,
     io::{self, Write},
 };
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let filepath = args[1].clone();
-
-    let content = fs::read_to_string(filepath).expect("Something went wrong reading the file");
-
-    let code_fmt = pattern::code_fmt(&content);
-    let mut code = code::Code::new(code_fmt);
-
-    io::stdout().write_all(code.format().as_bytes())?;
+    let options = cli::run();
+    let code_str =
+        fs::read_to_string(options.0).expect("Cannot read the input file");
+    let code_str = pattern::code_fmt(&code_str[..]);
+    let code = Code::new(code_str).format();
+    match options.1.len() {
+        0 => {
+            io::stdout().write_all(code.as_bytes())?;
+        }
+        _ => {
+            fs::write(options.1, code)?;
+        }
+    }
     Ok(())
 }
